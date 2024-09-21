e8a8979099ef3663e58b1246ec9a36a485d0652e
Bavly Amir <167124139+Bavly-Amir@users.noreply.github.com>
2024-09-21 12:36:45 +0300

    Edit

--- src/components/1-header/Nav1.jsx
+++ src/components/1-header/Nav1.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import './nav1.css';
-import logo from ".//../../images/Cover-1170x600-removebg-preview.png";
+import logo from ".//../../images/toppng.com-amazon-logo-transparent-768x293.png";
 
 export default function Nav1() {
   return (
     <div className="nav1 ">
       <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'transparent', color: 'inherit', width: '100%' }}>
         <div className="leftPart d-flex ">
-          <a className="navbar-brand" href="#" style={{ color: 'inherit' }}>
+          <a className="navbar-brand" href="#" style={{ color: 'inherit' , alignItems: 'normal', paddingLeft:'10px', paddingTop:'20px' }}>
             <img className='siteLogo' src={logo} alt="logo" />
+            <span style={{fontSize: '14px'}}>.eg</span>
           </a>
-        test
+
+          <div className="country d-flex" style={{ alignItems: "self-end" }}>
+
+            <div>
+              <i className="fa-solid fa-location-dot"></i>
+            </div>
+
+            <div id="glow-ingress-block" >
+              <span class="nav-line-1 nav-progressive-content" id="glow-ingress-line1">
+                Deliver to
+              </span>
+              <span class="nav-line-2 nav-progressive-content" id="glow-ingress-line2">
+                Egypt
+              </span>
+            </div>
+          </div>
+
+
+
         </div>
-      </nav>
+      </nav >
       <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'transparent', color: 'inherit', width: '100%' }}>
         <div className="  ">
           center
@@ -23,6 +42,6 @@ export default function Nav1() {
           right
         </div>
       </nav>
-    </div>
+    </div >
   );
 }
