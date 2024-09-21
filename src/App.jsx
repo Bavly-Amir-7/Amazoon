b932aec3d954edbe3cc2a9273f6bac6f63eaf9dd
Bavly Amir <167124139+Bavly-Amir@users.noreply.github.com>
2024-09-21 09:50:47 +0300

    Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-// this is App.js
-
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { useEffect } from 'react';
-import { Opacity } from '@mui/icons-material';
-
-
+import PersistentDrawerLeft from './components/1-header/Nav'; // Import the drawer component
 
 function App() {
-
-
-
   return (
-
-
     <>
-      <div id='up' className='container-fluid'>
-      
-
-
- 
-
+      <div id="up" className="container-fluid">
+        <PersistentDrawerLeft /> {/* Use the PersistentDrawerLeft component */}
       </div>
     </>
   );
